220ec66a6d1683d6065d7980e6c9ca3270ce8dad
[douglasvillegasvasquez] <douglas.villegas.vasquez1@gmail.com>
2020-05-13 01:03:36 -0300

    API DE FILMES

---
+++ src/main/java/br/usjt/movies_init/MoviesApplication.java
@@ -0,0 +1,12 @@
+package br.usjt.movies_init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+}
